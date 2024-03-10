b9462e83156c96a6c81f3b18cf247838aa7252e8
aadi58002 <aadi58002@gmail.com>
2024-03-10 21:29:21 +0530

    Change travis script and .gitignore

--- common/travis/changed_templates.sh
+++ common/travis/changed_templates.sh
@@ -12,7 +12,7 @@ case "$tip" in
 	*)         tip="${tip%% *}" ;;
 esac
 
-base="$(git merge-base origin/HEAD "$tip")"
+base="$(git merge-base upstream/master $tip)"
 
 [ $(git rev-list --count "$tip" "^$base") -lt 200 ] || {
 	echo "::error title=Branch out of date::Your branch is too out of date. Please rebase on upstream and force-push."
@@ -21,7 +21,6 @@ base="$(git merge-base origin/HEAD "$tip")"
 
 echo "$base $tip" >/tmp/revisions
 
-/bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
 git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	"$base" "$tip" \
 	-- 'srcpkgs/*/template' |
@@ -29,3 +28,18 @@ git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	xargs ./xbps-src sort-dependencies |
 	tee /tmp/templates |
 	sed "s/^/  /" >&2
+
+if [ -f ".github-actions-exclude.txt" ]; then
+   grep -vf .github-actions-exclude.txt /tmp/templates > /tmp/excluded-templates
+else
+   cat /tmp/templates > /tmp/excluded-templates
+fi
+
+if [ -f ".github-actions-include.txt" ]; then
+   cat /tmp/excluded-templates ./.github-actions-include.txt > /tmp/templates
+else
+   cat /tmp/excluded-templates > /tmp/templates
+fi
+
+/bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
+cat /tmp/templates
